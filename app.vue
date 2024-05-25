376ee1b381e7fda2e886d7c50c23c56d5ac6025e
Martin Fracker, Jr. <martin.frackerjr@gmail.com>
2024-05-25 07:20:15 -0500

    Fix title gen

--- app.vue
+++ app.vue
@@ -3,3 +3,13 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+useSeoMeta({
+  title: "Martin's server network",
+  ogTitle: "Martin's server network",
+  description: `Welcome to our Minecraft server network! Immerse yourself in a dynamic world where survival, camaraderie, and excitement await. Whether you’re a seasoned player or a curious newcomer, our server offers an unforgettable experience.`,
+  ogDescription:
+    "Welcome to our Minecraft server network! Immerse yourself in a dynamic world where survival, camaraderie, and excitement await. Whether you’re a seasoned player or a curious newcomer, our server offers an unforgettable experience.",
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: `Martin's server network`,
+      titleTemplate: (title) =>
+        title
+          ? `Martin's server network - ${title}`
+          : `Martin's server network`,
+    },
+  },
   devtools: { enabled: true },
   modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
   css: ["/assets/css/main.css"],
+  content: {
+    contentHead: false,
+  },
 });
